166fa0a6ccea2915b4d0bbaaa589ade7f5d6fc77
Thaer Assfour <thaer.assfour@gmail.com>
2020-12-27 20:50:07 +0200

    add details for splash

--- lib/pages/splash/splashScreen.dart
+++ lib/pages/splash/splashScreen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/pages/splash/splashListText.dart';
 import 'package:e_commerce/services/appLocal.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,7 +9,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  // add var to get list from splashTextList arabic or english
+  PageController pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     String languageCode =
@@ -16,24 +18,65 @@ class _SplashScreenState extends State<SplashScreen> {
     List splashList = splashListTextEN;
     if (languageCode == "ar") splashList = splashListTextAR;
 
-
     return Scaffold(
-        body: SafeArea(
-      child: Column(
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 10),
+                child: Text(
+                  "${getLang(context, "splashTitle")}",
+                  style: TextStyle(
+                      color: Color(0xff6990B9),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                )),
+            Expanded(
+              child: PageView.builder(
+                  controller: pageController,
+                  itemCount: splashList.length,
+                  itemBuilder: (context, index) {
+                    return SplashPageDetails(
+                        splashList[index]); //welcome widget
+                  }),
+            ),
+          ],
+        ));
+  }
+}
+
+class SplashPageDetails extends StatelessWidget {
+  final list;
+
+  SplashPageDetails(this.list);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height / 10,
-                  bottom: MediaQuery.of(context).size.height / 15),
-              child: Text(
-                "${getLang(context, "splashTitle")}",
-                style: TextStyle(
-                    color: Colors.lightBlue,
-                    fontSize: 30),
-              )),
-        ],
-      ),
-    ));
+      children: [
+        Image.asset(
+          list['image'],
+          width: MediaQuery.of(context).size.width / 1.2,
+        ),
+        Text(
+          list['title'],
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.orange, fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            list['description'],
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.grey, fontSize: 20, fontWeight: FontWeight.w200),
+          ),
+        ),
+      ],
+    );
   }
 }
